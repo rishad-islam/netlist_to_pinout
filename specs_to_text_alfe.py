1cfe8a8420e3dad99fa4c46d6b086cd478b78b2e
rishad-islam <159822971+rishad-islam@users.noreply.github.com>
2024-02-15 11:29:39 -0500

    Add files via upload

---
+++ specs_to_text_alfe.py
@@ -0,0 +1,51 @@
+import csv
+import re
+
+specs = open("MALFE1_part.edn","r")
+# these lines contain the info about ports, change them for your own use
+data = specs.read().splitlines()[418:1812]
+specs.close()
+
+# generates the list of ports with their raw data
+port_indexes = list()
+for i in range(len(data)):
+    if data[i].find("(port ") != -1:
+        port_indexes.append(i)
+ports_list = list()
+port_indexes.append(port_indexes[-1]+10) # adds a dummy entry to not break the next loop indexing
+for i in range(len(port_indexes)-1):
+    ports_list.append(data[port_indexes[i]:port_indexes[i+1]])
+
+# set search keywords for desired parameters, these should be changed if different data is desired
+direction_key = "direction "
+designator_key = "designator \""
+pintype_key = "PINTYPE  (string \""
+rows = list()
+for i in range(len(ports_list)):
+    # extracting pin name, direction, number (designator), pin type
+    # pin name in row 0
+    pin_row = ports_list[i][0]
+    pin_name = pin_row[pin_row.find("port")+5:].upper()
+    # iterating over other rows to account for differing formats
+    for j in range(len(ports_list[i])):
+        direction_index = ports_list[i][j].find(direction_key)
+        designator_index = ports_list[i][j].find(designator_key)
+        pintype_index = ports_list[i][j].find(pintype_key)
+        if  direction_index != -1:
+            pin_direction = ports_list[i][j][direction_index + len(direction_key):-1] # ending index is -1 or -2 depending on format
+        elif designator_index != -1:
+            pin_number = ports_list[i][j][designator_index + len(designator_key):-2]
+        elif pintype_index != -1:
+            pintype = ports_list[i][j][pintype_index + len(pintype_key):-2] 
+    pin_info = [pin_number, pin_name, pintype, pin_direction]
+    rows.append(pin_info)
+
+# sorting and file writing 
+rows.sort()
+header = ["Pin Number", "Pin Name", "Pin Type", "Pin Direction"]
+filename = "alfe_data.csv"
+with open(filename, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(header)
+    for i in range(len(ports_list)):
+        csvwriter.writerow(rows[i])
